e88e5aa9b68343196e0bdddf898403edad67bfbe
vanhalenar <thalenar@gmail.com>
2022-04-25 22:01:29 +0200

    finally figured sum stuff out

--- proj2.c
+++ proj2.c
@@ -12,27 +12,61 @@
 typedef struct
 {
 	sem_t mutex;
+	sem_t queue_h;
+	sem_t queue_o;
 }semaphores;
 //***********************
 
 #define smphSIZE sizeof(semaphores)
 
+//***********************
+typedef struct
+{
+	sem_t mutex;
+	sem_t turnstile1;
+	sem_t turnstile2;
+	int n;
+	int count;
+}barrier_t;
+
+//***********************
+
+#define brrSIZE sizeof(barrier_t)
+
+//***********************
+
+typedef struct
+{
+	int o;
+	int h;
+	int *o_ids;
+	int *h_ids;
+}queue_t;
+
+//***********************
+
+#define qSIZE sizeof(queue_t)
+
+//***********************
 //process for oxygen
 //***********************
-void oxygen_proc(semaphores *semaphore, int *cnt, int *o_cnt, int time)
+void oxygen_proc(semaphores *semaphore, int *cnt, int *o_cnt, int time, int moltime, queue_t *queue, int *mol_cnt, barrier_t *barrier)
 {
+	(void) mol_cnt;
 	srand(getpid());
-        int sleeptime = rand()%time+1;
+        int sleeptime = rand()%time;
+	srand(getpid());
+	int molsleeptime = rand()%moltime;
+	(void) molsleeptime;
 
 	sem_wait(&(semaphore->mutex));
 	
 	int id = ++(*o_cnt);
 	*cnt = *cnt + 1;
 
-
-
 	//correct print
 	printf("%d: O %d: started\n", *cnt, id);
+	fflush(stdout);
 	
 	//debug print
 	//printf("%d: O %d: started, sleep: %d, pid: %d, ppid: %d\n", *cnt, id, sleeptime, getpid(), getppid());
@@ -46,20 +80,79 @@ void oxygen_proc(semaphores *semaphore, int *cnt, int *o_cnt, int time)
 	
 	*cnt = *cnt + 1;
 
-	printf("%d: O %d: going to queue\n", *cnt, id);
-	
+	printf("%d: O %d: going to queue\n", *cnt, id);	
 	fflush(stdout);
 
-	sem_post(&(semaphore->mutex));
+	queue->o  = queue->o + 1;
+
+	queue->o_ids[queue->o-1] = id;
+
+	if (queue->h >= 2)
+	{
+		sem_post(&(semaphore->queue_h));
+		sem_post(&(semaphore->queue_h));
+		queue->h = queue->h - 2;
+		sem_post(&(semaphore->queue_o));
+		queue->o = queue->o - 1;
+
+	}
+	else
+	{
+		sem_post(&(semaphore->mutex));
+	}
+
+	sem_wait(&(semaphore->queue_o));
+	*cnt = *cnt + 1;
+	printf("%d: O %d: creating molecule 69\n", *cnt, id);
+	fflush(stdout);
+
+	//usleep(molsleeptime);
+
+	//***barrier.wait start***
+	sem_wait(&(barrier->mutex));
+	barrier->count = barrier->count + 1;
+	if (barrier->count == barrier->n)
+	{
+		sem_post(&(barrier->turnstile1));
+		sem_post(&(barrier->turnstile1));
+		sem_post(&(barrier->turnstile1));
+	}
+	sem_post(&(barrier->mutex));
+
+	sem_wait(&(barrier->turnstile1));
+
+	//***critical section start***
 	
+	printf("O %d: molecule 69 finished\n", id);
+	fflush(stdout);
+	
+	//***critical section end***
+	
+	sem_wait(&(barrier->mutex));
+	barrier->count = barrier->count - 1;
+	if (barrier->count == 0)
+	{
+		sem_post(&(barrier->turnstile2));
+		sem_post(&(barrier->turnstile2));
+		sem_post(&(barrier->turnstile2));
+	}
+	sem_post(&(barrier->mutex));
+	
+	sem_wait(&(barrier->turnstile2));
+
+	///***barrier.wait end***
+
+	sem_post(&(semaphore->mutex));
+
 	exit(0);
 }
 //***********************
 
 //process for hydrogen
 //***********************
-void hydrogen_proc(semaphores *semaphore, int *cnt, int *h_cnt, int time)
+void hydrogen_proc(semaphores *semaphore, int *cnt, int *h_cnt, int time, queue_t *queue, int *mol_cnt, barrier_t *barrier)
 {
+	(void) mol_cnt;
 	srand(getpid());
         int sleeptime = rand()%time+1;
 
@@ -70,6 +163,7 @@ void hydrogen_proc(semaphores *semaphore, int *cnt, int *h_cnt, int time)
 
 	//correct print
 	printf("%d: H %d: started\n", *cnt, id);
+	fflush(stdout);
 
 	//debug print
         //printf("%d: H %d: started, sleep: %d, pid: %d, ppid: %d\n", *cnt, id, sleeptime, getpid(), getppid());
@@ -86,7 +180,58 @@ void hydrogen_proc(semaphores *semaphore, int *cnt, int *h_cnt, int time)
 	printf("%d: H %d: going to queue\n", *cnt, id);
  	fflush(stdout);
 
-	sem_post(&(semaphore->mutex));
+	queue->h = queue->h + 1;
+
+	queue->h_ids[queue->h-1] = id;
+
+	if (queue->h >= 2 && queue->o >= 1)
+	{
+		sem_post(&(semaphore->queue_h));
+		sem_post(&(semaphore->queue_h));
+		queue->h = queue->h - 2;
+		sem_post(&(semaphore->queue_o));
+		queue->o = queue->o - 1;
+	}
+	else
+	{
+		sem_post(&(semaphore->mutex));
+	}
+
+	sem_wait(&(semaphore->queue_h));
+
+	//***barrier.wait start***
+	sem_wait(&(barrier->mutex));
+	barrier->count = barrier->count + 1;
+	if (barrier->count == barrier->n)
+	{
+		sem_post(&(barrier->turnstile1));
+		sem_post(&(barrier->turnstile1));
+		sem_post(&(barrier->turnstile1));
+	}
+	sem_post(&(barrier->mutex));
+
+	sem_wait(&(barrier->turnstile1));
+
+	//***critical section start***
+	
+	printf("H %d: molecule 69 finished\n", id);
+	fflush(stdout);
+	
+	//***critical section end***
+	
+	sem_wait(&(barrier->mutex));
+	barrier->count = barrier->count - 1;
+	if (barrier->count == 0)
+	{
+		sem_post(&(barrier->turnstile2));
+		sem_post(&(barrier->turnstile2));
+		sem_post(&(barrier->turnstile2));
+	}
+	sem_post(&(barrier->mutex));
+	
+	sem_wait(&(barrier->turnstile2));
+
+	///***barrier.wait end***
 
         exit(0);
 }
@@ -107,8 +252,8 @@ int main(int argc, char *argv[])
 	int atom_time;
 	atom_time = atoi(argv[3]);
 
-	//int molecule_time;
-	//molecule_time = atoi(argv[4]);
+	int molecule_time;
+	molecule_time = atoi(argv[4]);
 
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
@@ -125,9 +270,34 @@ int main(int argc, char *argv[])
 	h_cnt = mmap(NULL, intSIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	*h_cnt=0;
 
+	int *mol_cnt;
+	mol_cnt = mmap(NULL, intSIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	*mol_cnt=0;
+
+	queue_t *queue;
+	queue = mmap(NULL, qSIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	queue->o = 0;
+	queue->h = 0;
+
+	queue->o_ids = mmap(NULL, sizeof(int*)*(oxygen+1), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+
+	queue->h_ids = mmap(NULL, sizeof(int*)*(hydrogen+1), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+
 	semaphores *semaphore;
 	semaphore = mmap(NULL, smphSIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	sem_init(&(semaphore->mutex), 1, 1);
+	sem_init(&(semaphore->queue_o), 1, 0);
+	sem_init(&(semaphore->queue_h), 1, 0);
+
+	barrier_t *barrier;
+	barrier = mmap(NULL, brrSIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	sem_init(&(barrier->mutex), 1, 1);
+	sem_init(&(barrier->turnstile1), 1, 0);
+	sem_init(&(barrier->turnstile2), 1, 1);
+	barrier->n = 3;
+	barrier->count = 0;
 
 	pid_t pid = getpid();
 
@@ -139,7 +309,7 @@ int main(int argc, char *argv[])
 		}
 		if (pid == 0)
 		{
-			oxygen_proc(semaphore, cnt, o_cnt, atom_time);
+			oxygen_proc(semaphore, cnt, o_cnt, atom_time, molecule_time, queue, mol_cnt, barrier);
 		}
 	}
 
@@ -153,7 +323,7 @@ int main(int argc, char *argv[])
                 }
                 if (pid == 0)
                 {
-                        hydrogen_proc(semaphore, cnt, h_cnt, atom_time);
+                        hydrogen_proc(semaphore, cnt, h_cnt, atom_time, queue, mol_cnt, barrier);
                 }
         }
 
